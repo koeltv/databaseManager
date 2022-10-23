a0c945d5a55fb3a92bd859328c4bc8d160ace115
koeltv <Valentin.koeltgen@utt.fr>
2022-10-23 20:23:48 +0200

    !!! Temporarily changed org.xerial:sqlite-jdbc to a modified local one while waiting for pull request to be accepted, see [here](https://github.com/xerial/sqlite-jdbc/pull/799) !!!
    Created connectWithStatement method in DatabaseHelper to factorise the code
    Created RandomSQLValue to separate the generation of random values from DatabaseHelper
    Added nextSignedInt in Utils
    Reformatted test_create.sql and cinema_create.sql

--- src/main/kotlin/com/koeltv/databasemanager/Utils.kt
+++ src/main/kotlin/com/koeltv/databasemanager/Utils.kt
@@ -1,5 +1,12 @@
 package com.koeltv.databasemanager
 
+import kotlin.math.abs
+import kotlin.random.Random
+
 fun String.containsAny(vararg subStrings: String): Boolean {
     return subStrings.any { subString -> contains(subString) }
+}
+
+fun Random.nextSignedInt(from: Int, until: Int): Int {
+    return nextInt(abs(from) + until) - from
 }
